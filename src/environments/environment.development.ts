59b89123f6b2d10d6e97c34eb402290db3fb25ee
Milan Kumar Bhagat <milankrbhagat@gmail.com>
2026-05-06 15:49:42 +0530

    update guessing url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://staging-api.taskpulse.live',
+  apiUrl: 'https://staging.taskpulse.live',
   GOOGLE_CLIENT_ID: "347488313780-j6sckdktogbjo26ktf04cfsvmq456tkn.apps.googleusercontent.com",
   recaptchaSiteKey: "6Ldz3tAsAAAAAB3gaDNZp1o8pIz8xzZb0uvnbylK",
   logoUrl: 'icons/logo-staging.png'
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://staging-api.taskpulse.live', // Guessing the staging URL
+    apiUrl: 'https://staging.taskpulse.live', // Guessing the staging URL
     GOOGLE_CLIENT_ID: "347488313780-j6sckdktogbjo26ktf04cfsvmq456tkn.apps.googleusercontent.com",
     recaptchaSiteKey: "6Ldz3tAsAAAAAB3gaDNZp1o8pIz8xzZb0uvnbylK",
     logoUrl: 'icons/logo-staging.png'
