f8ef4830888f224b1cf9cf4484fa6d2ab6eed094
Milan Kumar Bhagat <milankrbhagat@gmail.com>
2026-05-06 15:22:40 +0530

    Differentiate staging and prod based on look

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api-dev.taskpulse.live',
-  GOOGLE_CLIENT_ID: "347488313780-j6sckdktogbjo26ktf04cfsvmq456tkn.apps.googleusercontent.com"
+  apiUrl: 'http://localhost:5000',
+  GOOGLE_CLIENT_ID: "347488313780-j6sckdktogbjo26ktf04cfsvmq456tkn.apps.googleusercontent.com",
+  logoUrl: 'icons/logo-staging.png'
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5000',
+    production: true,
+    apiUrl: 'https://staging-api.taskpulse.live', // Guessing the staging URL
     GOOGLE_CLIENT_ID: "347488313780-j6sckdktogbjo26ktf04cfsvmq456tkn.apps.googleusercontent.com",
     logoUrl: 'icons/logo-staging.png'
 };
