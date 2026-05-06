867cfff515d93c918a2796ca19a637a61940de4d
Milan Kumar Bhagat <milankrbhagat@gmail.com>
2026-05-07 00:00:06 +0530

    update env variables

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://api-dev.taskpulse.live',
   GOOGLE_CLIENT_ID: "347488313780-j6sckdktogbjo26ktf04cfsvmq456tkn.apps.googleusercontent.com",
+  recaptchaSiteKey: "6Ldz3tAsAAAAAB3gaDNZp1o8pIz8xzZb0uvnbylK",
   logoUrl: 'icons/logo-staging.png'
 };
