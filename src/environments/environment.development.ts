1c5ebcd0486b2c5e3226394c7bcac0ef8541fc18
Rupesh <rupesh737923@gmail.com>
2026-05-08 00:28:25 +0530

    TP-8: Display task details in popup
    New desgin changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api-dev.taskpulse.live',
+  apiUrl: 'http://localhost:5000',
   GOOGLE_CLIENT_ID: "347488313780-j6sckdktogbjo26ktf04cfsvmq456tkn.apps.googleusercontent.com"
 };
